8736765e4f2ec044d87afe4822469a87e82fe45f
Juhana Kaarlehto <juhana.kaa@gmail.com>
2025-01-27 14:38:45 +0200

    3.5

--- numbers/index.js
+++ numbers/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const assert = require('assert');
+const { warn } = require('console');
+
 const app = express()
+app.use(express.json())
 
 let persons = [
   {
@@ -30,6 +34,48 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.send(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(
+	`
+	<p> Phonebook has information about ${persons.length} people.</p>
+	<p>${Date()}</p>
+	`
+    )
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+	const id = request.params.id
+	const person = persons.find(person => person.id === id)
+	if (!person) response.status(404).end()
+	response.json(person)
+})
+
+app.delete('/api/persons/:id',(request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (!person) response.status(404).end()
+    persons = persons.filter(person => person.id != id)
+    assert(undefined === persons.find(person => person.id === id))
+    response.status(200).end()
+})
+
+app.post('/api/persons', (request,response) => {
+    const person = request.body
+    console.log(person)
+    if (persons.find(existingPerson => existingPerson.name === person.name)) {
+	response.status(409).end()
+    }
+
+    const id = Math.floor(Math.random() * 100000);
+    console.log(id)
+    person.id = id
+    persons = [...persons, person]
+
+    response.status(201).end()
+
 })
 
 const PORT = 3001
